ca97938244962c0d153afa30f904ad2df5e88d5a
Vinicius Silva <vini20_1@hotmail.com>
2018-10-27 14:39:19 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 	public static void main(String args[]){
 		Pessoa p = new Pessoa();
 		
-		p.insereDadosPessoa(1, "Vinicius Pereira", 19, "404.848.688.88", 1500);
+		p.insereDadosPessoa(1, "Vinicius Pereira", 19, "404", 1500);
 		p.mostraDadosPessoa(12);
 	}
 }
